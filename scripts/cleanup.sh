5827ea151dbf98c713067b68bd8e3a1a134c24fb
Lukasz Mateusz Pawlowski <lukasz.mateusz@namecheap.com>
2019-08-23 08:04:47 +0200

    Cleanup updated

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,6 +2,10 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+sudo -E sh -c 'systemctl disable systemd-resolved || true'
+sudo -E sh -c 'systemctl stop systemd-resolved || true'
+sudo -E sh -c 'rm -rf /etc/netplan || true'
+
 sudo apt-get clean
 sudo apt-get autoremove --yes
 
